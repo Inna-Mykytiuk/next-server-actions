c7d9e4ffa2cae8d36511b150f93b6192b4aae1d8
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-10-26 18:37:27 +0300

    add modal

--- src/api.ts
+++ src/api.ts
@@ -7,3 +7,33 @@ export const getAllTodos = async (): Promise<ITask[]> => {
   const todos = await res.json();
   return todos;
 };
+
+export const addTodo = async (todo: ITask): Promise<ITask> => {
+  const res = await fetch(`${baseUrl}/tasks`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(todo),
+  });
+  const newTodo = await res.json();
+  return newTodo;
+};
+
+export const editTodo = async (todo: ITask): Promise<ITask> => {
+  const res = await fetch(`${baseUrl}/tasks/${todo.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(todo),
+  });
+  const updatedTodo = await res.json();
+  return updatedTodo;
+};
+
+export const deleteTodo = async (id: string): Promise<void> => {
+  await fetch(`${baseUrl}/tasks/${id}`, {
+    method: "DELETE",
+  });
+};
--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,13 +1,37 @@
+"use client";
 import { AiOutlinePlus } from 'react-icons/ai';
+import { Modal } from './Modal';
+import { FormEventHandler, useState } from "react";
+import { useRouter } from "next/navigation";
+import { addTodo } from '@/utils/api-utils/todo-api';
+import { v4 as uuidv4 } from "uuid";
 
 
 const AddTask = () => {
+  const router = useRouter();
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [newTaskValue, setNewTaskValue] = useState<string>("");
+
+  // const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = async (e) => {
+  //   e.preventDefault();
+  //   await addTodo({
+  //     id: uuidv4(),
+  //     text: newTaskValue,
+  //   });
+  //   setNewTaskValue("");
+  //   setModalOpen(false);
+  //   router.refresh();
+  // };
+
   return (
     <div>
-      <button className="btn btn-primary w-full">
+      <button
+        className="btn btn-primary w-full"
+        onClick={() => setModalOpen(true)}>
         Add new task
         <AiOutlinePlus size={18} className="ml-2" />
       </button>
+      <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>Add ToDo</Modal>
     </div>
   )
 }
