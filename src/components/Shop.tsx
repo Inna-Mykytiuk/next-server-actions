feb5d9d5096ff26e86a1a2631c00c942e45f11c9
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-10-31 22:48:43 +0200

    fixing

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -5,34 +5,12 @@ import Image from 'next/image'
 import { useAppSelector, useAppDispatch } from "../../redux/hooks";
 
 const Shop = () => {
-  const data = [
-    {
-      id: 1,
-      title: "Samsung Galaxy A51",
-      price: "350",
-      image_url: "https://procell.pe/wp-content/uploads/2020/10/a51.png",
-    },
-    {
-      id: 2,
-      title: "Samsung Galaxy A12",
-      price: "150",
-      image_url:
-        "https://cdn.ycan.shop/stores/3b145e8182eb3d8cbdd6633fd0df4d38/products/mfc0KT7Cq53NqdCLRPwjTUp75zpoV2NLmNMXEG52.png",
-    },
-    {
-      id: 3,
-      title: "Samsung Galaxy S21",
-      price: "450",
-      image_url:
-        "https://cdn.ycan.shop/stores/3b145e8182eb3d8cbdd6633fd0df4d38/products/mfc0KT7Cq53NqdCLRPwjTUp75zpoV2NLmNMXEG52.png",
-    },
-  ];
-  // const data = useAppSelector((state) => {
-  //   console.log("Full state:", state);
-  //   return state.shop.data || [];
-  // });
+  const data = useAppSelector((state) => {
+    console.log("Full state:", state);
+    return state.shop.data || [];
+  });
 
-  // console.log("Data:", data);
+  console.log("Data:", data);
   return (
     <div id="grid">
       {data.map((item, i) => (
